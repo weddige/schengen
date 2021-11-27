7f728a921870f72558722b2d1ffb8506d3f0b140
Konstantin Weddige <konstantin@skathi.net>
2021-11-27 23:47:04 +0100

    Add type hints

--- schengen.py
+++ schengen.py
@@ -1,55 +1,63 @@
+import warnings
 from datetime import date, timedelta
+from typing import List, Tuple
 
 
-class OverstayError(Exception):
+class VisaError(Exception):
+    pass
+
+
+class OverstayWarning(UserWarning):
     pass
 
 
 class Visa:
-    def __init__(self, valid_from, valid_until, duration_of_stay, type_of_visa):
-        assert type_of_visa == "C", "Only type C visas are supported"
+    """This class represents a schengen type C visa."""
+
+    def __init__(self, valid_from: date, valid_until: date):
         self.valid_from = valid_from
         self.valid_until = valid_until
-        self.duration_of_stay = duration_of_stay
-        self.trips = []
+        self._duration_of_stay = timedelta(days=90)
+        self._trips: List[Tuple[date, date]] = []
 
-    def add_trip(self, entry, exit):
-        assert exit < self.valid_until, "Stay exeeds validity of visa"
-        self.trips.append((entry, exit))
-
-    def __str__(self):
+    def __str__(self) -> str:
         result = ""
-        for entry, exit in self.trips:
+        for entry, exit in self._trips:
             result += f"{entry} {exit} {exit-entry+timedelta(days=1)}\n"
         return result
 
-    def evaluate(self, day):
-        result = self.duration_of_stay
-        for entry, exit in self.trips:
+    def add_trip(self, entry: date, exit: date, strict: bool = True) -> None:
+        assert exit < self.valid_until, "Stay exeeds validity of visa"
+        self._trips.append((entry, exit))
+        result = self._duration_of_stay
+        for days in range((exit - entry).days + 1):
+            day = entry + timedelta(days=days)
+            result = min(result, self.evaluate(day))
+        if result <= timedelta(0):
+            if strict:
+                del self._trips[-1]
+                raise VisaError(f"Trip could not be added because of overstay by {-result}")
+            else:
+                warnings.warn(f"Overstay in the period from {entry} to {exit} by {-result}", OverstayWarning)
+
+    def evaluate(self, day: date):
+        result = self._duration_of_stay
+        for entry, exit in self._trips:
             if exit < (start := day - timedelta(days=180)):
                 pass
             else:
                 result -= min(exit, day) - max(entry, start) + timedelta(days=1)
         return result
 
-    def validate(self):
-        for entry, exit in self.trips:
+    @property
+    def valid(self) -> bool:
+        for entry, exit in self._trips:
             overstay = timedelta(days=1)
             for days in range((exit - entry).days + 1):
                 day = entry + timedelta(days=days)
                 if (tmp := self.evaluate(day)) <= timedelta(0):
                     overstay = min(overstay, tmp)
             if overstay <= timedelta(0):
-                raise OverstayError(f"Overstay in the period from {entry} to {exit} by {overstay}")
+                warnings.warn(f"Overstay in the period from {entry} to {exit} by {-overstay}", OverstayWarning)
+                return False
         return True
-
-    def plan(self, entry, exit):
-        self.trips.append((entry, exit))
-        print(self)
-        for days in range((exit - entry).days + 1):
-            day = entry + timedelta(days=days)
-        result = self.evaluate(exit)
-        del self.trips[-1]
-        if result <= timedelta(0):
-            raise OverstayError(f"Overstay in the period from {entry} to {exit} by {result}")
-        return result
